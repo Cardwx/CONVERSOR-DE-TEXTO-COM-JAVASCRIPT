851d9a0a9ec7687bd2184cc8cfe1af004da78d40
Cardwx <lcb.barbeiro@gmail.com>
2022-05-24 17:40:56 -0300

    conversão ok
    conversão dos dados para maiúsculo/min feito, nos próximos dias será dada a continuidade nos demais componentes de aplicação

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const mai = document.querySelector('.btn-mai')
 const min = document.querySelector('.btn-min')
 const limpar = document.querySelector('.btn-clear')
+
 /**
  * Entrada das funções para conversão
  */
@@ -8,29 +9,32 @@ const limpar = document.querySelector('.btn-clear')
 //Função da conversão para maiusculo
 
 function  maiusculo(){
-    const  texto = document.querySelector('.texto').value
-    const texto_convertido = document.querySelector('.texto-conver')
-
+    const  texto = document.querySelector('#texto').value
+    console.log(texto)
     // manipulação do dom
-    document.querySelector('.texto-conver').innerHTML = texto.toUpperCase()  
+    document.querySelector('#texto').value = texto.toUpperCase() 
+
+    
+
 }
 //Função da conversão para minusculo
 
 function  minusculo(){
-    const  texto = document.querySelector('.texto').value
+    const  texto = document.querySelector('#texto').value
     const texto_convertido = document.querySelector('.texto-conver')
 
     // manipulação do dom
-    document.querySelector('.texto-conver').innerHTML = texto.toLowerCase() 
+    document.querySelector('#texto').value = texto.toLowerCase() 
 }
 
 // função de limpeza do texto convertido
 function  limpeza(){
-    const  texto = document.querySelector('.texto').value
+    const  texto = document.querySelector('#texto').value
     const texto_convertido = document.querySelector('.texto-conver')
 
     // manipulação do dom
-    document.querySelector('.texto-conver').innerHTML = " " 
+    document.querySelector('textarea#texto').value = " " 
+    
 }
 
 limpeza.click
